c3dbbaf7aa12f925b4590cd317deb4ff24de16a6
pupungeggang6 <pupungeggang6@gmail.com>
2025-02-18 01:41:40 +0900

    Card

--- Card/Game/Script/Data.js
+++ Card/Game/Script/Data.js
@@ -1,4 +1,4 @@
 const dataCard = {
-    1: {'ID': 1, 'Name': 'Card 1', 'Type': 'Unit', 'Rarity': 'Basic', 'Element': 'any', 'Energy': ['Any'], 'Stat': [1, 2], 'Action': []},
-    2: {'ID': 2, 'Name': 'Card 2', 'Type': 'Unit', 'Rarity': 'Basic', }
+    1: {'ID': 1, 'Name': 'Card 1', 'Type': 'Unit', 'Rarity': 'Basic', 'Element': 'any', 'Energy': ['Any'], 'Stat': [1, 2], 'Action': [['Summon', 'This']]},
+    2: {'ID': 2, 'Name': 'Card 2', 'Type': 'Unit', 'Rarity': 'Basic', 'Element': 'any', 'Energy': ['Any', 'Any'], 'Stat': [2, 3], 'Action': [['Summon', 'This']]}
 }
