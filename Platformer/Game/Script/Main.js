5d17c3f0a0b1d5caed7f267f59bdd5a5bf6f22e1
pupungeggang6 <pupungeggang6@gmail.com>
2025-02-23 15:18:25 +0900

    Temp

---
+++ Platformer/Game/Script/Main.js
@@ -0,0 +1,62 @@
+window.onload = main
+window.onerror = errorHandle
+window.oncontextmenu = rightClick
+
+function main() {
+    canvas = document.getElementById('screen')
+    context = canvas.getContext('2d')
+
+    window.addEventListener('keydown', keyDown, false)
+    window.addEventListener('keyup', keyUp, false)
+
+    frameCurrent = Date.now()
+    gameLoop = requestAnimationFrame(loop)
+}
+
+function loop() {
+    framePrevious = frameCurrent
+    frameCurrent = Date.now()
+    delta = frameCurrent - framePrevious
+
+    if (scene === 'title') {
+        loopTitle()
+    } else if (scene === 'platformer') {
+        loopPlatformer()
+    }
+
+    gameLoop = requestAnimationFrame(loop)
+}
+
+function keyDown(event) {
+    let key = event.key
+
+    if (state === 'start') {
+        state = ''
+    }
+
+    if (scene === 'title') {
+        keyDownTitle(key)
+    } else if (scene === 'platformer') {
+        keyDownPlatformer(key)
+    }
+}
+
+function keyUp(event) {
+    let key = event.key
+
+    if (scene === 'title') {
+        keyUpTitle(key)
+    } else if (scene === 'platformer') {
+        keyUpPlatformer(key)
+    }
+}
+
+function errorHandle(err, url, line, col, obj) {
+    if (obj != null) {
+        cancelAnimationFrame(gameLoop)
+    }
+}
+
+function rightClick() {
+    return false
+}
---
+++ Platformer/Game/Script/Var.js
@@ -0,0 +1,11 @@
+let canvas
+let context
+
+let gameLoop
+let frameCurrent
+let framePrevious
+let delta
+
+let scene = 'title'
+let state = 'start'
+let menu = false
