e4bc90e6bd3dddca39234b21cb5cf3c35332def9
pupungeggang6 <pupungeggang6@gmail.com>
2025-03-02 23:55:49 +0900

    Temp:

---
+++ CardPay/CardPay.js
@@ -0,0 +1,19 @@
+class Slot {
+    energy  = []
+    constructor(energy) {
+
+    }
+}
+
+class Energy {
+
+}
+
+function cardPay(cost, current) {
+    let costCopy = JSON.parse(JSON.stringify(cost))
+    let currentCopy = JSON.parse(JSON.stringify(current))
+
+    
+
+    return currentCopy
+}
