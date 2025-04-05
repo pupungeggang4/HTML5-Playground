bb0d607f73f7bccbcaa97a9eb49787acafd61765
pupungeggang6 <pupungeggang6@gmail.com>
2025-04-05 15:02:42 +0900

    Reset Projects

--- Card/Game/Script/Asset.js
+++
@@ -1,9 +0,0 @@
-let img = {
-    portal: new Image(),
-    portalToggle: new Image(),
-}
-
-function imageLoad() {
-    img.portal.src = 'Image/Portal.png'
-    img.portalToggle.src = 'Image/PortalToggle.png'
-}
--- Platformer/Game/Script/UI.js
+++
@@ -1,8 +0,0 @@
-const UI = {
-    title: {
-        textTitle: [24, 24],
-        textStart: [184, 184],
-        textErase: [184, 264],
-        arrow: [[80, 160], [80, 240]],
-    }
-}
